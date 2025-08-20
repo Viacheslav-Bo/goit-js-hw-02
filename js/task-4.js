6c808db3723e6e1625696a0bd437461c5779ff10
Viacheslav-Bo-PC <zotikivna@gmail.com>
2025-08-20 22:45:25 +0300

    1,2,4 tasks

--- js/task-4.js
+++ js/task-4.js
@@ -1,4 +1,23 @@
-getShippingCost(country);
+function getShippingCost(country) {
+  let message;
+  switch (country) {
+    case 'China':
+      message = 'Shipping to China will cost 100 credits';
+      break;
+    case 'Chile':
+      message = 'Shipping to Chile will cost 250 credits';
+      break;
+    case 'Australia':
+      message = 'Shipping to Australia will cost 170 credits';
+      break;
+    case 'Jamaica':
+      message = 'Shipping to Jamaica will cost 120 credits';
+      break;
+    default:
+      message = 'Sorry, there is no delivery to your country';
+  }
+  return message;
+}
 
 console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost('Germany')); // "Sorry, there is no delivery to your country"
